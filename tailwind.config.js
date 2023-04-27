50198bf6458f23c76b8a4d9a0bf0fbd9014412f2
kabi02 <108052970+kabi02@users.noreply.github.com>
2023-04-27 14:43:22 +0800

    fixed fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'opensans': ['Open Sans', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
