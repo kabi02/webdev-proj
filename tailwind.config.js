0f0ad352f082e62681b8273921492f49be24f6d8
valeriannnne <valerie.liann@gmail.com>
2023-04-28 23:32:57 +0800

    main div

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+
+// sm	640px	@media (min-width: 640px) { ... }
+// md	768px	@media (min-width: 768px) { ... }
+// lg	1024px	@media (min-width: 1024px) { ... }
+// xl	1280px	@media (min-width: 1280px) { ... }
+// 2xl	1536px
+
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
