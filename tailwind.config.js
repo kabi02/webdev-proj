5f2900d0d19fad50f6ab3f7cb4a974f469b31820
valeriannnne <valerie.liann@gmail.com>
2023-04-28 14:16:33 +0800

    improved navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
+    extend: {      
+      fontSize: {
+        xs: '0.1rem',
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
       fontFamily: {
         'opensans': ['Open Sans', 'sans-serif']
       },
