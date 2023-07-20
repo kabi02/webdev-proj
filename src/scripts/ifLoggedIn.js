0da00c991227b79d610e0eff92e5afeedf31f356
yong <yong.ursua@gmail.com>
2023-07-20 23:36:12 +0800

    spag codes
    naka auth na pag naka sign in sa lahat ng page
    
    comments editable only if logged in

--- src/scripts/ifLoggedIn.js
+++ src/scripts/ifLoggedIn.js
@@ -26,15 +26,16 @@
     // Get the content div element
     const content = document.getElementById("content");
     const phone = document.getElementById("content-phone");
+    const comment = document.getElementById("comment");
 
     // Listen to the authentication state changes
     onAuthStateChanged(auth, (user) => {
         if (user) {
             // User is logged in
             content.innerHTML = `
-            <div class="border-2 border-black text-center items-center">
+            <div class="text-center items-center">
                 <h1 class="font-bold">${user.email} <i class="fa-solid fa-angle-down cursor-pointer" id="arrow"></i></h1> 
-                <button id="logout" class="hidden border-2 border-black mt-5 p-2">Logout</button>
+                <button id="logout" class="hidden mt-5 p-2">Logout</button>
             </div>
             `;
 
@@ -64,6 +65,24 @@
                 } 
             });
 
+            comment.innerHTML = `
+            <div class="bg-white px-3 rounded-md font-opensans pb-1">
+                <h1 class="font-extrabold text-[24px] text-gray-800">Comments</h1>
+                <div class="relative my-2">
+                    <div class="absolute inset-y-0 left-0 flex pl-3 pt-2.5 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
+                    </div>
+                    <textarea id="subject" name="message" autocomplete="off" rows="5" cols="50" class="bg-[#D9D9D9] resize-none border border-gray-300 text-[#808080] focus:text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" 
+                    placeholder="Post a comment... "></textarea>
+                    <div class="mt-2">
+                    <button id="btn-comment" type="submit" class="border-none bg-black text-white text-base 
+                    h-10 w-full rounded-lg hover:bg-gray-800 transition: duration-200">Submit</button>
+                    </div>
+                </div>
+
+            </div>    
+            `
+
         } else {
             // User is logged out
             content.innerHTML = `
@@ -76,6 +95,19 @@
                 <a class="block px-4 py-3 mb-3 leading-none text-xs text-center font-semibold bg-gray-50 hover:bg-gray-100 rounded-xl" href="sign_in.html">Sign in</a>
                 <a class="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-black hover:bg-gray-500 rounded-xl" href="sign_up.html">Sign Up</a>
             </div>
+            `;
+
+            comment.innerHTML = `
+            <div class="bg-white px-3 rounded-md font-opensans pb-1">
+                <h1 class="font-extrabold text-[24px] text-gray-800">Comments</h1>
+                <div class="relative my-2">
+                    <div class="absolute inset-y-0 left-0 flex pl-3 pt-2.5 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
+                    </div>
+                    <textarea id="subject" name="message" autocomplete="off" rows="5" cols="50" class="cursor-not-allowed bg-[#D9D9D9] resize-none border border-gray-300 text-[#808080] focus:text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" 
+                    placeholder="You must be logged in to post a comment... " disabled></textarea>
+                </div>
+            </div>    
             `
         }
     });
